43db4b4f7cb9583add35d581aacd1755743a89a5
Flames <vibe-coder@flames.com>
2025-11-14 06:08:31 +0000

    Update project prj_vzonux9w - 2025-11-14T06:08:30.603Z

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,12 @@ import App from './App'
 import Test from './Test'
 import './index.css'
 
+// Render without StrictMode to avoid double-mount side effects with some WebGL/3D libs (e.g., Spline)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/test" element={<Test />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
+  </BrowserRouter>,
 )
